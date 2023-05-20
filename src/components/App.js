c95c6155ab9bea2fb4a36fd8244994ac313b02a8
rahulbarik23 <rahul.barik1996@gmail.com>
2023-05-21 00:06:17 +0530

    packege change

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 
+import React,{useState} from "react";
 import React from "react";
 import './../styles/App.css';
 
+
 const App = () => {
+  let [btnClicked, countBtnClicked] = useState(0);
   return (
     <div>
-        {/* Do not remove the main div */}
+         <p>Button clicked {btnClicked} times</p>
+        <button onClick={() => {countBtnClicked(btnClicked + 1)}}>Click me</button>
     </div>
   )
 }
